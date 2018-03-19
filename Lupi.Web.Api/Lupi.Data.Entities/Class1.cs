f6f3efb938d47d5300dfdd41b99016c2cbc35ed2
Santiago <santial89@gmail.com>
2018-03-19 14:24:10 -0300

    creo primeros proyectos vacíos

---
+++ Lupi.Web.Api/Lupi.Data.Entities/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lupi.Data.Entities
+{
+    public class Class1
+    {
+    }
+}
---
+++ Lupi.Web.Api/Lupi.Web.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lupi.Web.Api.WebApiApplication" Language="C#" %>
