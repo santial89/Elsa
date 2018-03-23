9cafdc9cfdac4645f730d85df38583c28eaf519a
Santiago <santial89@gmail.com>
2018-03-23 02:00:55 -0300

    Agrego Proyecto BusinessLogic

--- Lupi.Web.Api/Lupi.Web.Api.Models/BreedBusinessLogic.cs
+++ Lupi.Web.Api/Lupi.Web.Api.Models/BreedBusinessLogic.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lupi.Web.Api.Models
+namespace Lupi.BusinessLogic
 {
-    public class Class1
+    public class BreedBusinessLogic
     {
+
+
     }
 }
