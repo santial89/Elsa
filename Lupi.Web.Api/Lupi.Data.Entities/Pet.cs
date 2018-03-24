22e1a24d6e39a58e084cbe1ee02ac831afad3ade
Andrés Bentos <sebita.uy@gmail.com>
2018-03-24 13:01:29 -0300

    Se agrega prueba 'PetPropertiesNombreOK' y dicha properties Name en Pet

---
+++ Lupi.Web.Api/Lupi.Data.Entities/Pet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lupi.Data.Entities
+{
+    public class Pet
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Lupi.Web.Api/Lupi.UnitTest/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lupi.Data.Entities;
+
+namespace Lupi.UnitTest
+{
+    [TestClass]
+    public class PetUnitTest
+    {
+        [TestMethod]
+        public void PetCrearInstanciaOK()
+        {
+            //Arrange
+            Pet aPet;
+            //Act
+            aPet = new Pet();
+            //Assert
+            Assert.IsNotNull(aPet);
+        }
+        [TestMethod]
+        public void PetPropertiesGuidOK()
+        {
+            //Arrange
+            Guid unGuid = new Guid();
+            Pet aPet = new Pet();
+            //Act
+            aPet.Id = unGuid;
+            //Assert
+            Assert.AreEqual(aPet.Id, unGuid);
+        }
+        [TestMethod]
+        public void PetPropertiesNameOK()
+        {
+            //Arrange
+            String unNombre = "UNNOMBRE";
+            Pet aPet = new Pet();
+            //Act
+            aPet.Name = unNombre;
+            //Assert
+            Assert.AreEqual(aPet.Name, unNombre);
+        }
+    }
+}
