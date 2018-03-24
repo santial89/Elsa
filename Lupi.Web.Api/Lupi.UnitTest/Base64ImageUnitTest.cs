6814668a70fc2834e7cc202856aab56a6fc285e3
Andrés Bentos <sebita.uy@gmail.com>
2018-03-24 11:54:37 -0300

    Crea test Base64Image inicial

---
+++ Lupi.Web.Api/Lupi.UnitTest/Base64ImageUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lupi.UnitTest
+{
+    /// <summary>
+    /// Descripción resumida de Base64Image
+    /// </summary>
+    [TestClass]
+    public class Base64ImageUnitTest
+    {
+        public Base64ImageUnitTest()
+        {
+            //
+            // TODO: Agregar aquí la lógica del constructor
+            //
+        }
+
+       
+       
+
+        [TestMethod]
+        public void CrearCrearInstancia()
+        {
+            //Arrange
+            Base64Image imagen; 
+            //Act
+            imagen= new Base64Image();
+            //Assert
+            Assert.IsNotNull(imagen);
+        }
+    }
+}
