1676d29137f5068455023a1a23aba021a31f6ccb
Andrés Bentos <sebita.uy@gmail.com>
2018-03-25 11:33:31 -0300

    Agrega Contexto en Lupi.Data.DataAccess

---
+++ Lupi.Web.Api/Lupi.Data.DataAccess/LupiDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lupi.Data.Entities;
+
+namespace Lupi.Data.DataAccess
+{
+    public class LupiDbContext : DbContext
+    {
+        public LupiDbContext() : base("name=Lupi") { }
+
+        public DbSet<Breed> Breeds { get; set; }
+        public DbSet<Base64Image> Images { get; set; }
+        public DbSet<Collar> Collars { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+    }
+}
