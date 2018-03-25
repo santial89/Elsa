1a01569be376107a41a462af100685346a85c0ea
Andrés Bentos <sebita.uy@gmail.com>
2018-03-25 15:50:31 -0300

    agrego el resto

---
+++ Lupi.Web.Api/Lupi.Data.DataAccess/LupiContext.cs
@@ -0,0 +1,59 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lupi.Data.Entities;
+
+namespace Lupi.Data.DataAccess
+{
+    public class LupiContext : DbContext
+    {
+        public LupiContext():base("name=LupiDBConnectionString")
+        {
+            /*
+            drops an existing database and creates a new database, if your model classes 
+            (entity classes) have been changed. So, you don't have to worry about maintaining 
+            your database schema, when your model classes change
+            Más info en: http://www.entityframeworktutorial.net/code-first/database-initialization-strategy-in-code-first.aspx
+            
+            También es posible configurar todo esto en AppConfig. Se hace acá para que esté junto. Luego se puede cambiar.
+            */
+            Database.SetInitializer<LupiContext>(new DropCreateDatabaseIfModelChanges<LupiContext>());
+        }
+ 
+        public DbSet<Breed> Breeds { get; set; }
+        public DbSet<Base64Image> Images { get; set; }
+        public DbSet<Collar> Collars { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Acá va la configuración de FluentApi
+            //Esquema por defecto
+            modelBuilder.HasDefaultSchema("EsquemaLUPI");
+            //Mapear Entidades
+            modelBuilder.Entity<Breed>().ToTable("Breeds");
+            modelBuilder.Entity<Base64Image>().ToTable("Images");
+            modelBuilder.Entity<Collar>().ToTable("Collars");
+            modelBuilder.Entity<Owner>().ToTable("Owners");
+            modelBuilder.Entity<Pet>().ToTable("Pets");
+            //Claves primarias
+            modelBuilder.Entity<Breed>().HasKey(B => B.id);
+            modelBuilder.Entity<Base64Image>().HasKey(B => B.Id);
+            modelBuilder.Entity<Collar>().HasKey(C => C.Id);
+            modelBuilder.Entity<Owner>().HasKey(O => O.Id);
+            modelBuilder.Entity<Pet>().HasKey(P => P.Id);
+            //Restricciones estructurales (relaciones).
+            modelBuilder.Entity<Collar>().HasRequired<Owner>(c => c.Owner).WithMany(o => o.Collars).HasForeignKey(c => c.OwnerId);
+            modelBuilder.Entity<Owner>().HasMany<Pet>(c => c.Pets);
+            modelBuilder.Entity<Pet>().HasOptional<Collar>(P => P.AssignedCollar);
+            modelBuilder.Entity<Pet>().HasRequired<Breed>(P => P.Breed);
+            modelBuilder.Entity<Pet>().HasRequired<Base64Image>(P => P.Image);
+
+
+           
+           
+        }
+    }
+
+}
---
+++ Lupi.Web.Api/Lupi.Web.Api/Controllers/BreedsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Lupi.Data.DataAccess;
+using Lupi.Data.Entities;
+
+namespace Lupi.Web.Api.Controllers
+{
+    public class BreedsController : ApiController
+    {
+        private LupiContext db = new LupiContext();
+
+        // GET: api/Breeds
+        public IQueryable<Breed> GetBreeds()
+        {
+            return db.Breeds;
+        }
+
+        // GET: api/Breeds/5
+        [ResponseType(typeof(Breed))]
+        public IHttpActionResult GetBreed(Guid id)
+        {
+            Breed breed = db.Breeds.Find(id);
+            if (breed == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(breed);
+        }
+
+        // PUT: api/Breeds/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutBreed(Guid id, Breed breed)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != breed.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(breed).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BreedExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Breeds
+        [ResponseType(typeof(Breed))]
+        public IHttpActionResult PostBreed(Breed breed)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Breeds.Add(breed);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (BreedExists(breed.id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = breed.id }, breed);
+        }
+
+        // DELETE: api/Breeds/5
+        [ResponseType(typeof(Breed))]
+        public IHttpActionResult DeleteBreed(Guid id)
+        {
+            Breed breed = db.Breeds.Find(id);
+            if (breed == null)
+            {
+                return NotFound();
+            }
+
+            db.Breeds.Remove(breed);
+            db.SaveChanges();
+
+            return Ok(breed);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool BreedExists(Guid id)
+        {
+            return db.Breeds.Count(e => e.id == id) > 0;
+        }
+    }
+}
